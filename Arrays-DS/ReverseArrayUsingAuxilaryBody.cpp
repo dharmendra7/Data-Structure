bda12903924833d33b597bcc66505834e881a246
dharmendra <dharmeshrathod8080@gmail.com>
2023-12-18 15:01:12 +0530

    Recursive Binary Search

--- Arrays-DS/ReverseArrayUsingAuxilaryBody.cpp
+++ Arrays-DS/ReverseArrayUsingAuxilaryBody.cpp
@@ -7,22 +7,31 @@ struct Array{
     int lenght;
 };
 
-int main() {
-    Array arr = {{1,3,4,5,6,7,8,9,11,13,16,17,18,33}, 15, 14};
-    int brr[14];
+void Reverse(Array *arr)
+{
     int i, j;
-    for(i  = arr.lenght -1, j = 0; i>=0; i--, j++)
+    int brr[14];
+    for(i=arr->lenght-1, j = 0; i>=0; i--, j++)
     {
-        brr[j] = arr.A[i];
+        brr[j] = arr->A[i];
     }
-    
-    for( i =0; i<arr.lenght; i++)
+    for(i=0; i<arr->lenght; i++)
     {
-        arr.A[i] = brr[i];
+        arr->A[i] = brr[i];
     }
-    for( i =0; i<arr.lenght; i++)
+}
+
+void Display(Array arr)
+{
+    for(int i=0; i<arr.lenght; i++)
     {
         cout<<arr.A[i]<<" ";
     }
+}
+
+int main() {
+    Array arr = {{1,3,4,5,6,7,8,9,11,13,16,17,18,33}, 15, 14};
+    Reverse(&arr);
+    Display(arr);
     return 0;
 }
