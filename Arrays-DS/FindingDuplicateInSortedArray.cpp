954156bf1c2533f1b8ded3c31a6d3a6aa62a8285
dharmendra <dharmeshrathod8080@gmail.com>
2023-12-21 16:56:29 +0530

    finding duplicate in sorted array

---
+++ Arrays-DS/FindingDuplicateInSortedArray.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+using namespace std;
+
+class Array{
+    private:
+        int *A;
+        int size;
+        int length;
+        
+    public:
+        Array(int sz)
+        {
+            size = sz;
+            length = 0;
+            A = new int[sz];
+        }
+        
+        void Append(int x)
+        {
+            if(length < size)
+                A[length++] = x;
+        }
+       
+        void FindDuplicateElements()
+        {
+            int last_duplicate = 0;
+            for(int i=0; i<length-1; i++)
+            {
+                if(A[i]==A[i+1] && A[i]!=last_duplicate)
+                {
+                    cout<<A[i]<<" ";
+                    last_duplicate = A[i];
+                }
+            }
+        }
+};
+
+int main()
+{
+    int size;
+    cout<<"Enter the size: ";
+    cin>>size;
+    
+    Array arr(size);
+    
+    cout<<"Enter the elements: \n";
+    for(int i=0; i<size; i++)
+    {
+        int x;
+        cin>>x;
+        arr.Append(x);
+    }
+    cout<<"Duplicate Elements are: ";
+    arr.FindDuplicateElements();
+}
