db23d19564f610590ae73588a019cefc5d4c88ea
dharmendra <dharmeshrathod8080@gmail.com>
2023-12-21 13:55:24 +0530

    Added class based approach

--- Arrays-DS/DifferentiateOperationInArray.cpp
+++ Arrays-DS/DifferentiateOperationInArray.cpp
@@ -53,3 +53,106 @@ int main()
     Display(*arr3);
     return 0;
 }
+
+
+/*Using class*/
+
+#include <iostream>
+using namespace std;
+
+class Array
+{
+private:
+    int *A;
+    int size;
+    int length;
+
+public:
+    Array(int sz)
+    {
+        size = sz;
+        length = 0;
+        A = new int[size];
+    }
+
+    ~Array()
+    {
+        delete[] A;
+    }
+
+    void Append(int x)
+    {
+        if (length < size)
+            A[length++] = x;
+    }
+
+    Array *Difference(Array *arr2)
+    {
+        int i, j, k;
+        i = j = k = 0;
+
+        Array *arr3 = new Array(size);
+
+        while (i < length && j < arr2->length)
+        {
+            if (A[i] < arr2->A[j])
+                arr3->Append(A[i++]);
+            else if (arr2->A[j] < A[i])
+                j++;
+            else
+            {
+                i++;
+                j++;
+            }
+        }
+
+        for (; i < length; i++)
+            arr3->Append(A[i]);
+
+        return arr3;
+    }
+
+    void Display()
+    {
+        for (int i = 0; i < length; i++)
+            cout << A[i] << " ";
+    }
+};
+
+int main()
+{
+    int size1, size2;
+
+    cout << "Enter the size of Array 1: ";
+    cin >> size1;
+    Array arr1(size1);
+
+    cout << "Enter elements for Array 1:\n";
+    for (int i = 0; i < size1; i++)
+    {
+        int x;
+        cin >> x;
+        arr1.Append(x);
+    }
+
+    cout << "Enter the size of Array 2: ";
+    cin >> size2;
+    Array arr2(size2);
+
+    cout << "Enter elements for Array 2:\n";
+    for (int i = 0; i < size2; i++)
+    {
+        int x;
+        cin >> x;
+        arr2.Append(x);
+    }
+
+    Array *arr3 = arr1.Difference(&arr2);
+
+    cout << "Difference: ";
+    arr3->Display();
+
+    delete arr3;
+
+    return 0;
+}
--- Arrays-DS/IntersectionOperationInArray.cpp
+++ Arrays-DS/IntersectionOperationInArray.cpp
@@ -49,3 +49,101 @@ int main()
     Display(*arr3);
     return 0;
 }
+
+
+/*Intersection using class*/
+
+#include <iostream>
+using namespace std;
+
+class Array{
+    private:
+        int *A;
+        int size;
+        int length;
+        
+    public:
+        Array(int sz)
+        {
+            size = sz;
+            length = 0;
+            A = new int[size];
+        }
+        
+        ~Array()
+        {
+            delete[] A;
+        }
+        
+        void Append(int x)
+        {
+            if(length<size)
+                A[length++] = x;
+        }
+        
+        Array *Intersection(Array *arr2)
+        {
+            int i, j, k;
+            i=j=k = 0;
+            Array *arr3 = new Array(size);
+            
+            while(i<length && j<arr2->length)
+            {
+                if(A[i]<arr2->A[j])
+                    i++;
+                else if(arr2->A[j]<A[i])
+                    j++;
+                else
+                {
+                    arr3->A[k++] = A[i++];
+                    j++;
+                }
+            }
+            arr3->length = k;
+            
+            return arr3;
+        }
+        
+        void Display()
+        {
+            for(int i=0; i<length; i++)
+                cout<<A[i]<<" ";
+        }
+};
+
+
+int main()
+{
+    int size1, size2;
+    cout<<"Enter the size of array 1: ";
+    cin>>size1;
+    
+    Array arr1(size1);
+    cout<<"Enter the elements for array 1: \n";
+    for(int i=0; i<size1; i++)
+    {
+        int x;
+        cin>>x;
+        arr1.Append(x);
+    }
+    
+    cout<<"Enter the size of array 2: ";
+    cin>>size2;
+    
+    Array arr2(size2);
+    cout<<"Enter the elements for array 2: \n";
+    for(int i=0; i<size2; i++)
+    {
+        int x;
+        cin>>x;
+        arr2.Append(x);
+    }
+    
+    Array *arr3 = arr1.Intersection(&arr2);
+    
+    cout<<"Intersection: ";
+    arr3->Display();
+    
+    delete arr3;
+    return 0;
+}
