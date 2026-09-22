9462ab2d2f32fa41080508d593459e2a7f9d9e47
dharmendra7 <dmrathod8080@gmail.com>
2026-09-22 22:31:28 +0530

    Integrated the code for the diagonal matrix

--- Matrices/DiagonalMatrix.cpp
+++ Matrices/DiagonalMatrix.cpp
@@ -1,72 +1,56 @@
 #include <iostream>
 using namespace std;
 
-class Diagonal
-{
-    private:
+class Diagonal {
+    private: 
         int n;
-        int *A;
+        int* A;
     public:
-        Diagonal()
-        {
-            n = 2;
-            A = new int[2];
-        }
-        
-        Diagonal(int n)
-        {
+        Diagonal(int n) {
             this->n = n;
             A = new int[n];
+        };
+
+        void set(int i, int j, int x) {
+            if (i == j) {
+                A[i - 1] = x;
+            }
+        };
+
+        int get(int i, int j) {
+            if (i == j) {
+                return A[i - 1];
+            } else {
+                return 0;
+            }
+        };
+
+        void display() {
+            for (int i = 0; i < n; i++){
+                for (int j = 0; j < n; j++) {
+                    if (i == j) {
+                        cout << A[i] << " ";
+                    } else {
+                        cout << "0 ";
+                    }
+                }
+                cout << endl;
+            }
         }
-        
-        void set(int i, int j, int x);
-        int get(int i, int j);
-        void Display();
-        
-        ~Diagonal()
-        {
-            delete []A;
-        }
-        
+
+        ~Diagonal() {
+            delete[] A;
+        };
 };
 
-void Diagonal::set(int i, int j, int x)
-{
-    if(i==j)
-        A[i-1] = x;
-}
-
-int Diagonal::get(int i, int j)
-{
-    if(i==j)
-        return A[i-1];
-    else
-        return 0;
-}
-void Diagonal::Display()
-{
-    for(int i=0; i<n; i++)
-    {
-        for(int j=0; j<n; j++)
-        {
-            if(i==j)
-                cout<<A[i]<<" ";
-            else
-                cout<<"0 ";
-        }
-        cout<<endl;
-    }
-}
-
-int main()
-{
+int main() {
     Diagonal d(4);
-    d.set(1,1,2);
-    d.set(2,2,4);
-    d.set(3,3,6);
-    d.set(4,4,8);
-    
-    d.Display();
-    
+    d.set(1, 1, 5);
+    d.set(2, 2, 8);
+    d.set(3, 3, 9);
+    d.set(4, 4, 12);
+
+    d.display();
+
     return 0;
 }
